ff0946b0a3e45dfbeb903d97cccf08680d422bea
dqnk <64268180+dqnk@users.noreply.github.com>
2022-10-12 21:45:17 +0200

    some work done

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,17 @@ use std::process::Command;
 fn main() {
     //automatically reads in current dir
     let paths = fs::read_dir("./").unwrap();
-    let output = Command::new("java").arg("Test.java").output().expect("run");
-    println!("status: {}", output.status);
-    io::stdout().write_all(&output.stdout).unwrap();
-    io::stderr().write_all(&output.stderr).unwrap();
-
     for path in paths {
-        println!("Name: {}", path.unwrap().path().display());
+        let file = path.unwrap().path().display().to_string();
+        let file_name = String::from(file);
+        if file_name.ends_with(".in") {
+            let output = Command::new("java").arg("Test.java").output().expect("run");
+            println!("status: {}", output.status);
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+        } else {
+            println!("No input files");
+            return;
+        }
     }
 }
