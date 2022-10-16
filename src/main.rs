3a83c81afe9dd58499006638123025d6a2d3fd73
dqnk <64268180+dqnk@users.noreply.github.com>
2022-10-16 12:13:30 +0200

    some stuff will have to be done with lifetimes, boxes and references

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,53 @@
 use std::env;
+use std::ffi::OsStr;
 use std::fs;
 use std::io::{self, Write};
 use std::path::Path;
 use std::process::Command;
 
-fn find_main_java(path: &Path) {
-    let paths = fs::read_dir(path).unwrap();
+fn find_main_java<'a>(pat: Box<&Path>) -> Box<&'a Path> {
+    let paths = fs::read_dir(*pat).unwrap();
     for path in paths {
-        let file = path.unwrap().path().display().to_string();
-        let file_name = String::from(file);
-        println!("{}", file_name);
-
-        if file_name.ends_with(".java") {
-            file_name;
-        }
+        //        let file2 = Path::new(&path.unwrap().path().display().to_string());
+        //       let file = file2.clone().extension().and_then(OsStr::to_str);
+        let pat2 = Box::new(Path::new(&path.unwrap().path().display().to_string()));
+        return pat2;
     }
+    panic!("error finding main.java file");
 }
 
 fn main() {
     //automatically reads in current dir
     let args: Vec<String> = env::args().collect();
-    let mut test_dir: &Path;
-    let program_name: String;
+    let test_dir: &Path;
+    let program_name: &Path;
     match args.len() {
-        1 => test_dir = Path::new("."),
-        2 => test_dir = Path::new("."),
-        _ => println!("Error"),
+        1 => {
+            test_dir = Path::new(".");
+            let test_box = Box::new(test_dir);
+            program_name = find_main_java(test_box);
+        }
+        2 => {
+            if args[1].ends_with(".java") {
+                test_dir = Path::new(".");
+                program_name = Path::new(&args[1]);
+            } else {
+                test_dir = Path::new(&args[1]);
+                program_name = find_main_java(&test_dir);
+            }
+        }
+        _ => {
+            test_dir = Path::new("a");
+            program_name = find_main_java(&test_dir);
+            println!("Error");
+        }
     }
+
+    println!(
+        "{}, {}",
+        String::from(test_dir.display().to_string()),
+        String::from(program_name.display().to_string()),
+    );
     if args.len() != 3 {
         println!("wrong input");
         return;
@@ -40,6 +61,7 @@ fn main() {
         test_dir.display().to_string(),
         program_name
     );
+    return;
 
     let paths = fs::read_dir(test_dir).unwrap();
 
