a62a119657dd97a1ecd4b82cac1147f3362ed948
dqnk <64268180+dqnk@users.noreply.github.com>
2022-10-16 17:59:18 +0200

    comments and includes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::ffi::OsStr;
 use std::fs;
+use std::io::{self, Write};
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
@@ -23,26 +24,43 @@ fn find_main_java(path: &Path) -> PathBuf {
 }
 
 fn main() {
-    //automatically reads in current dir
+    // read args provided to command from CLI
     let args: Vec<String> = env::args().collect();
     let test_dir: &Path;
     let program_name: PathBuf;
+
+    // obtain test_dir and program_name depending on the args provided
+    // args.len() returns number of args including the executable name stored in &args[0]
     match args.len() {
         1 => {
+            //assume everything is happening in current dir
             test_dir = Path::new(".");
             program_name = find_main_java(test_dir);
         }
         2 => {
+            // java program is provided, tests are in current dir
             if args[1].ends_with(".java") {
                 test_dir = Path::new(".");
                 program_name = PathBuf::from(&args[1]);
             } else {
+                // test dir is provided, java program is in current dir
                 test_dir = Path::new(&args[1]);
-                program_name = find_main_java(test_dir);
+                program_name = find_main_java(Path::new("."));
+            }
+        }
+        3 => {
+            // java program and test dir are provided, order does not matter
+            // should program name be exected without ".java" filetype
+            if args[1].ends_with(".java") {
+                test_dir = Path::new(&args[2]);
+                program_name = PathBuf::from(&args[1]);
+            } else {
+                test_dir = Path::new(&args[1]);
+                program_name = PathBuf::from(&args[2]);
             }
         }
         _ => {
-            panic!("error")
+            panic!("too many/too few arguments")
         }
     }
 
@@ -52,20 +70,18 @@ fn main() {
         String::from(program_name.display().to_string()),
     );
 
-    return;
+    let paths = fs::read_dir(test_dir).unwrap();
 
-    //    let paths = fs::read_dir(test_dir).unwrap();
-    //
-    //    for path in paths {
-    //        let file = path.unwrap().path().display().to_string();
-    //        let file_name = String::from(file);
-    //        println!("{}", file_name);
-    //
-    //        if file_name.ends_with(".in") {
-    //            let output = Command::new("java").arg("Test.java").output().expect("run");
-    //            println!("status: {}", output.status);
-    //            io::stdout().write_all(&output.stdout).unwrap();
-    //            io::stderr().write_all(&output.stderr).unwrap();
-    //        }
-    //    }
+    for path in paths {
+        let file = path.unwrap().path().display().to_string();
+        let file_name = String::from(file);
+        println!("{}", file_name);
+
+        if file_name.ends_with(".in") {
+            let output = Command::new("java").arg("Test.java").output().expect("run");
+            println!("status: {}", output.status);
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+        }
+    }
 }
