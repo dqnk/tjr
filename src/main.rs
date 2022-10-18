1933e40a8d9151f894710ce832531c5976641490
dqnk <64268180+dqnk@users.noreply.github.com>
2022-10-18 20:56:06 +0200

    stuff kinda works, but code needs serious cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(let_else)]
 use std::env;
 use std::ffi::OsStr;
 use std::fs;
@@ -52,46 +53,52 @@ fn main() {
             // java program and test dir are provided, order does not matter
             // should program name be exected without ".java" filetype
             if args[1].ends_with(".java") {
-                test_dir = Path::new(&args[2]);
                 program_name = PathBuf::from(&args[1]);
+                test_dir = Path::new(&args[2]);
             } else {
-                test_dir = Path::new(&args[1]);
-                program_name = PathBuf::from(&args[2]);
+                program_name = PathBuf::from(format!("{}.java", &args[1]));
+                test_dir = Path::new(&args[2]);
             }
         }
         _ => {
             panic!("too many/too few arguments")
         }
     }
+    let mut i = 0;
+    //contains all files, not just tests, but will be filtered later in for loop
+    let tests = fs::read_dir(test_dir).unwrap().into_iter();
 
-    println!(
-        "{}, {}",
-        String::from(test_dir.display().to_string()),
-        String::from(program_name.display().to_string()),
-    );
+    for test in tests {
+        let file = test.unwrap().path();
+        if file.extension() == None {
+            continue;
+            //TODO need serious cleanup here
+        } else {
+            if file.extension().unwrap() == "in" {
+                let output = Command::new("java")
+                    .arg(&program_name)
+                    .output()
+                    .expect("run");
+                // TODO: write output to .res file
 
-    let paths = fs::read_dir(test_dir).unwrap();
-
-    for path in paths {
-        let file = path.unwrap().path().display().to_string();
-        let file_name = String::from(file);
-        println!("{}", file_name);
-
-        if file_name.ends_with(".in") {
-            let output = Command::new("java").arg("Test.java").output().expect("run");
-            // TODO: write output to .res file
-
-            // panics should probably just be a println
-            if output.status.code().unwrap() < 0 {
-                io::stderr().write_all(&output.stderr).unwrap();
-                panic!("Something is wrong with your OS: error {}", output.status);
-            } else if output.status.code().unwrap() > 0 {
-                io::stderr().write_all(&output.stderr).unwrap();
-                panic!("Error compiling/running: {}", output.status);
-            } else {
-                println!("status: {}", output.status);
-                io::stdout().write_all(&output.stdout).unwrap();
+                // panics should probably just be a println
+                if output.status.code().unwrap() < 0 {
+                    io::stderr().write_all(&output.stderr).unwrap();
+                    panic!("Something is wrong with your OS: error {}", output.status);
+                } else if output.status.code().unwrap() > 0 {
+                    io::stderr().write_all(&output.stderr).unwrap();
+                    panic!("Error compiling/running: {}", output.status);
+                } else {
+                    fs::write(
+                        format!("{}.out", file.file_stem().unwrap().to_str().unwrap()),
+                        &output.stdout,
+                    )
+                    .expect("Unable to write output file");
+                }
             }
         }
+        println!("{}", i);
+        i += 1;
     }
+    println!("here");
 }
