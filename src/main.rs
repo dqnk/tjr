2756f917a0dcc528670664f04e660c0477caf76b
dqnk <64268180+dqnk@users.noreply.github.com>
2022-10-19 22:20:43 +0200

    fixing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use async_std;
-use std::ffi::OsStr;
 use std::fs;
 use std::fs::File;
 use std::io::{self, Write};
@@ -8,25 +7,61 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
-fn find_main_java(path: &Path) -> PathBuf {
-    let paths = fs::read_dir(path).unwrap();
+fn find_main_java(path: &Path) -> Result<PathBuf, io::Error> {
+    let paths = fs::read_dir(path).expect("Could not find target test dir").into_iter();
     for p in paths {
-        if p.as_ref()
-            .unwrap()
-                .path()
-                .as_path()
-                .extension()
-                .and_then(OsStr::to_str)
-                == Some("java")
-        {
-            return p.unwrap().path();
+        let file = match p {
+            Ok(file) => file.path(),
+            Err(error) => return Err(error),
+        };
+        match file.extension(){
+            Some(extension) => {
+                if extension == "java" {
+                    return Ok(file);
+                }
+            },
+            None => {},
         }
     }
     panic!("error finding main.java file");
 }
 
+async fn thread(program_name: PathBuf, file: &Path) {
+    let file_stem = file.file_stem().unwrap().to_str().unwrap();
+    let output = Command::new("java")
+        .arg(&program_name)
+        .stdin(File::open(format!("{}.in", file_stem)).unwrap())
+        .output()
+        .expect("run");
+    // panics should probably just be a println
+    if output.status.code().unwrap() < 0 {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Something is wrong with your OS: error {}", output.status);
+    } else if output.status.code().unwrap() > 0 {
+        io::stderr().write_all(&output.stderr).unwrap();
+        panic!("Error compiling/running: {}", output.status);
+    } else {
+        fs::write(format!("{}.res", file_stem), &output.stdout)
+            .expect("Unable to write output file");
+        let out_file = PathBuf::from(format!("{}.out", file_stem));
+        let res_file = PathBuf::from(format!("{}.res", file_stem));
+        let output_diff = Command::new(format!("diff"))
+            .arg(&out_file)
+            .arg(&res_file)
+            .output()
+            .expect("run");
+        let out = String::from_utf8(output_diff.stdout).unwrap();
+        if out == "" {
+            println!("fine");
+        } else {
+            println!("not fine {}", out);
+        }
+    }
+
+}
+
 #[async_std::main]
-async fn main() {
+async fn main() -> Result<(), io::Error>{
     // read args provided to command from CLI
     let args: Vec<String> = env::args().collect();
     let test_dir: &Path;
@@ -39,7 +74,7 @@ async fn main() {
         1 => {
             //assume everything is happening in current dir
             test_dir = Path::new(".");
-            program_name = find_main_java(test_dir);
+            program_name = find_main_java(test_dir)?;
         }
         2 => {
             // java program is provided, tests are in current dir
@@ -49,7 +84,7 @@ async fn main() {
             } else {
                 // test dir is provided, java program is in current dir
                 test_dir = Path::new(&args[1]);
-                program_name = find_main_java(Path::new("."));
+                program_name = find_main_java(Path::new("."))?;
             }
         }
         3 => {
@@ -64,63 +99,36 @@ async fn main() {
             }
         }
         _ => {
-            panic!("too many/too few arguments")
+            panic!("too many arguments")
         }
     }
+    let folder = fs::read_dir(test_dir).expect("error").into_iter();
     //contains all files, not just tests, but will be filtered later in for loop
-    let tests = fs::read_dir(test_dir).unwrap().into_iter();
+    for file in folder {
 
-    for test in tests {
-        let program_name = program_name.clone();
-        let file = PathBuf::from(test.unwrap().path());
-        if file.extension() == None {
-            continue;
-            //TODO need serious cleanup here
-        } else {
-            // TODO pipe the input file
-            if file.extension().unwrap() == "in" {
+        let file = match file {
+            Ok(file) => file.path(),
+            Err(error) => return Err(error),
+        };
+
+        match file.extension(){
+            Some(extension) => {if extension == "in" {
                 children.push(async_std::task::spawn({
                     let program_name = program_name.clone();
                     let file = file.clone();
+                    //TODO which asyncs are necessary here?
                     async move {
-                    let file_stem = file.file_stem().unwrap().to_str().unwrap();
-                    let output = Command::new("java")
-                        .arg(&program_name)
-                        .stdin(File::open(format!("{}.in", file_stem)).unwrap())
-                        .output()
-                        .expect("run");
-                    // TODO: write output to .res file
-
-                    // panics should probably just be a println
-                    if output.status.code().unwrap() < 0 {
-                        io::stderr().write_all(&output.stderr).unwrap();
-                        panic!("Something is wrong with your OS: error {}", output.status);
-                    } else if output.status.code().unwrap() > 0 {
-                        io::stderr().write_all(&output.stderr).unwrap();
-                        panic!("Error compiling/running: {}", output.status);
-                    } else {
-                        fs::write(format!("{}.res", file_stem), &output.stdout)
-                            .expect("Unable to write output file");
-                        //diff should return nothing
-                        let out_file = PathBuf::from(format!("{}.out", file_stem));
-                        let res_file = PathBuf::from(format!("{}.res", file_stem));
-                        let output_diff = Command::new(format!("diff"))
-                            .arg(&out_file)
-                            .arg(&res_file)
-                            .output()
-                            .expect("run");
-                        let out = String::from_utf8(output_diff.stdout).unwrap();
-                        if out == "" {
-                            println!("fine");
-                        } else {
-                            println!("not fine {}", out);
-                        }
-                    }
-                }}));
-            }
+                        let _ = thread(program_name, &file).await;
+                    }}));
+            }},
+            None => {},
         }
     }
     for child in children {
         let _ = child.await;
     }
+    return Ok(());
 }
+//for child in children {
+//    let _ = child.await;
+//}
