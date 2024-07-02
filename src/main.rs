79f15a45d4927de987b4bf44fd3c28394dc789cd
dqnk <64268180+dqnk@users.noreply.github.com>
2024-07-02 18:23:35 +0200

    more fixes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use async_std;
 use std::env;
 use std::ffi::OsStr;
 use std::fs;
@@ -9,30 +8,30 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn find_main_java(path: &PathBuf) -> Result<PathBuf, io::Error> {
-    let paths = fs::read_dir(path)?.into_iter();
+    let paths = fs::read_dir(path)?;
     for p in paths {
         let file = p?.path();
         if file.extension().unwrap_or(OsStr::new(".")) == "java" {
             return Ok(file);
         }
     }
-    return Err(Error::new(ErrorKind::Other, "Main java file not found."));
+    Err(Error::new(ErrorKind::Other, "Main java file not found."))
 }
 
 fn io_thread(t_idx: &u8, program_name: &PathBuf, file: &Path) -> Result<String, io::Error> {
     //this should be simpler
     let output = Command::new("java")
         .arg("-cp")
-        .arg(&program_name.parent().unwrap_or(Path::new(".")))
+        .arg(program_name.parent().unwrap_or(Path::new(".")))
         //this probably needs a better solution than OsStr::new
         .arg(
-            &program_name
+            program_name
                 .file_name()
                 .unwrap_or(OsStr::new(&program_name)),
         )
         .stdin(File::open(file)?)
         .output()?;
-    return handle_diffs(file, &output, t_idx);
+    handle_diffs(file, &output, t_idx)
 }
 
 fn class_thread(
@@ -43,9 +42,10 @@ fn class_thread(
 ) -> Result<String, io::Error> {
     let output = Command::new("java")
         .current_dir(program_dir)
-        .arg(&program_name)
+        .arg(program_name)
         .output()?;
-    return handle_diffs(file, &output, t_idx);
+
+    handle_diffs(file, &output, t_idx)
 }
 
 fn handle_diffs(
@@ -54,43 +54,47 @@ fn handle_diffs(
     t_idx: &u8,
 ) -> Result<String, io::Error> {
     let output_status = output.status.code().unwrap_or(-1);
-    if output_status < 0 {
-        io::stderr().write_all(&output.stderr)?;
-        return Err(Error::new(
-            ErrorKind::Other,
-            format!("Error {} from java - program could not run", output_status),
-        ));
-    } else if output_status > 0 {
-        io::stderr().write_all(&output.stderr)?;
-        return Err(Error::new(
-            ErrorKind::Other,
-            format!(
-                "Error {} from java - error compiling probably",
-                output_status
-            ),
-        ));
-    } else {
-        fs::write(file.with_extension("res"), &output.stdout)?;
-
-        let output_diff = Command::new("diff")
-            .arg("--context")
-            .arg(file.with_extension("res"))
-            .arg(file.with_extension("out"))
-            .output()?;
-        if output_diff.stderr.is_empty() && output_diff.stdout.is_empty() {
-            return Ok(String::from(format!("\u{2705} Test {}", t_idx)));
-        } else if output_diff.stdout.is_empty() {
-            return Err(Error::new(
+    match output.status.code().unwrap_or(-1).cmp(&0) {
+        std::cmp::Ordering::Less => {
+            io::stderr().write_all(&output.stderr)?;
+            Err(Error::new(
                 ErrorKind::Other,
-                "Error - no .out file probably",
-            ));
-        } else {
-            fs::write(
-                file.with_extension("diff"),
-                String::from_utf8(output_diff.stdout)
-                    .unwrap_or(String::from("Output and result files differ")),
-            )?;
-            return Ok(String::from(format!("\u{274C} Test {}", t_idx)));
+                format!("Error {} from java - program could not run", output_status),
+            ))
+        }
+        std::cmp::Ordering::Greater => {
+            io::stderr().write_all(&output.stderr)?;
+            Err(Error::new(
+                ErrorKind::Other,
+                format!(
+                    "Error {} from java - error compiling probably",
+                    output_status
+                ),
+            ))
+        }
+        _ => {
+            fs::write(file.with_extension("res"), &output.stdout)?;
+
+            let output_diff = Command::new("diff")
+                .arg("--context")
+                .arg(file.with_extension("res"))
+                .arg(file.with_extension("out"))
+                .output()?;
+            if output_diff.stderr.is_empty() && output_diff.stdout.is_empty() {
+                Ok(format!("\u{2705} Test {}", t_idx))
+            } else if output_diff.stdout.is_empty() {
+                return Err(Error::new(
+                    ErrorKind::Other,
+                    "Error - no .out file probably",
+                ));
+            } else {
+                fs::write(
+                    file.with_extension("diff"),
+                    String::from_utf8(output_diff.stdout)
+                        .unwrap_or(String::from("Output and result files differ")),
+                )?;
+                return Ok(format!("\u{274C} Test {}", t_idx));
+            }
         }
     }
 }
@@ -101,7 +105,7 @@ async fn test_class(
 ) -> Result<Vec<String>, std::io::Error> {
     let mut t_idx = 1;
     let mut children = vec![];
-    let folder = fs::read_dir(test_dir)?.into_iter();
+    let folder = fs::read_dir(test_dir)?;
 
     let programs = fs::read_dir(program_dir)?
         .filter_map(|p| p.ok())
@@ -143,10 +147,7 @@ async fn test_class(
                         .to_lowercase(),
                 );
 
-                async move {
-                    let a = class_thread(&t_idx, &program_name, &file_name, &program_dir);
-                    return a;
-                }
+                async move { class_thread(&t_idx, &program_name, &file_name, &program_dir) }
             }));
             t_idx += 1;
         }
@@ -159,23 +160,17 @@ async fn test_class(
     Ok(outs)
 }
 
-async fn test_io(
-    program_name: &PathBuf,
-    test_dir: &PathBuf,
-) -> Result<Vec<String>, std::io::Error> {
+async fn test_io(program_name: &Path, test_dir: &PathBuf) -> Result<Vec<String>, std::io::Error> {
     let mut t_idx = 1;
     let mut children = vec![];
-    let folder = fs::read_dir(test_dir)?.into_iter();
+    let folder = fs::read_dir(test_dir)?;
     for file in folder {
         let file = file?.path();
         if file.extension().unwrap_or(OsStr::new("")) == "in" {
             children.push(async_std::task::spawn({
-                let program_name = program_name.clone().with_extension("");
+                let program_name = program_name.with_extension("");
                 let file = file.clone();
-                async move {
-                    let a = io_thread(&t_idx, &program_name, &file);
-                    return a;
-                }
+                async move { io_thread(&t_idx, &program_name, &file) }
             }));
             t_idx += 1;
         }
@@ -185,7 +180,7 @@ async fn test_io(
         let out = child.await.unwrap_or(String::from("Something went wrong"));
         outs.push(out);
     }
-    return Ok(outs);
+    Ok(outs)
 }
 
 #[async_std::main]
@@ -229,27 +224,31 @@ async fn main() -> Result<(), io::Error> {
     if program_name.is_dir() {
         // multiple test classes
         children = test_class(&program_name, &test_dir).await?;
+        for child in children {
+            println!("{}", child);
+        }
+
+        Ok(())
     } else {
         let _output = Command::new("javac")
             .arg(&program_name.with_extension("java"))
             .output()?;
         if _output.stderr.is_empty() {
             children = test_io(&program_name, &test_dir).await?;
+            for child in children {
+                println!("{}", child);
+            }
+
+            Ok(())
         } else {
-            return Err(Error::new(
+            Err(Error::new(
                 ErrorKind::Other,
                 format!(
                     "Error compiling java \n{}",
                     String::from_utf8(_output.stderr)
                         .unwrap_or(String::from("No error message available from javac")),
                 ),
-            ));
+            ))
         }
     }
-
-    for child in children {
-        println!("{}", child);
-    }
-
-    Ok(())
 }
